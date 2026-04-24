25c4fd3fd6b0d5f81d1f0c6bf4cc3597487d2db6
luiGarcg <luisggcosta2006@gmail.com>
2026-04-24 18:18:04 -0300

    adjust a url

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://proj-marriage.vercel.app"
+    "https://proj-marriage2.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
